14d9066b6fa34491e5d98e210240f262777de81d
Tiago <tiagohslino@gmail.com>
2025-02-14 11:02:19 -0300

    created header and js axios

---
+++ script.js
@@ -0,0 +1,7 @@
+const userInfo = {
+    name: "Joao",     
+};
+
+//Enviando a resposta para o servidor
+
+const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/5e8669ec-b289-4b8d-b330-604f94b66969", userInfo);
