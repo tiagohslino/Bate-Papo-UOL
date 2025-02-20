a84a695ac9979bdd5c26eacf387b24f6434f69eb
Tiago <tiagohslino@gmail.com>
2025-02-20 11:33:48 -0300

    add footer

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ const userInfo = {
 //Enviando a resposta para o servidor
 
 const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/5e8669ec-b289-4b8d-b330-604f94b66969", userInfo);
+console.log(promise);
