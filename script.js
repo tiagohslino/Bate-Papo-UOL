07a40deee8960504813d3ccec4091442cd4cc500
Tiago <tiagohslino@gmail.com>
2025-02-22 13:00:02 -0300

    js innerhtml

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-const userInfo = {
-    name: "Joao",     
-};
+const userInfo = [
+    {name: "Joao"}
+];
 
 //Enviando a resposta para o servidor
 
 const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/5e8669ec-b289-4b8d-b330-604f94b66969", userInfo);
 console.log(promise);
+
+const message = document.querySelector(".main-chat");
+message.innerHTML = "";
+
+message.innerHTML += `Mensagem de teste ${userInfo[0].name}`;
