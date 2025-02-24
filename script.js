98d5f54cde9c514d955eb2660511141e9bf912f3
tiagohslino <tiagohslino@gmail.com>
2025-02-24 19:37:58 -0300

    rd4

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-const userInfo = [
-    {name: "Joao"}
-];
+const allUsers =     
+    {name: "fazer o queeee", age: "22"};
 
-//Enviando a resposta para o servidor
-
-const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/5e8669ec-b289-4b8d-b330-604f94b66969", userInfo);
+const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/cfa360a9-8b93-4bd9-b601-debbb28197a4", allUsers);
 console.log(promise);
-
-const message = document.querySelector(".main-chat");
-message.innerHTML = "";
-
-message.innerHTML += `Mensagem de teste ${userInfo[0].name}`;
