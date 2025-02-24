558a671e8c9d91380f13bb53bb4c728ee9c1a4fc
Tiago <tiagohslino@gmail.com>
2025-02-24 17:44:31 -0300

    with error name ok but age no

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const participants = [
+    { name: "okjaojao", age: 11 },
+    { name: "ok", age: 22 },
+  ];
 
-const yourData = {
-name: "okjaojao"};
-
-axios.post('https://mock-api.driven.com.br/api/v6/uol/participants/5e8669ec-b289-4b8d-b330-604f94b66969/', yourData)
-   
+  participants.forEach(participant => {
+    axios.post('https://mock-api.driven.com.br/api/v6/uol/participants/a9c569cf-b76f-4a31-88b1-fb7da3f1e93f/', participant)
+      .then(response => {
+        // Lide com a resposta da API para este participante
+      })
+      .catch(error => {
+        // Lide com erros para este participante
+      });
+  });
