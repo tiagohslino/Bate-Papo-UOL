95c5e07b5dcd6cb78e3c7b7e4e672cb43cbb5eee
Tiago <tiagohslino@gmail.com>
2025-02-24 11:57:12 -0300

    fixing axios error 400

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
-const userInfo = [
-    {name: "Joao"}
-];
 
-//Enviando a resposta para o servidor
+const yourData = {
+name: "okjaojao"};
 
-const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants/5e8669ec-b289-4b8d-b330-604f94b66969", userInfo);
-console.log(promise);
-
-const message = document.querySelector(".main-chat");
-message.innerHTML = "";
-
-message.innerHTML += `Mensagem de teste ${userInfo[0].name}`;
+axios.post('https://mock-api.driven.com.br/api/v6/uol/participants/5e8669ec-b289-4b8d-b330-604f94b66969/', yourData)
+   
